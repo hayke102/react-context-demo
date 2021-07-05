bd4cc7916e9a9b0011608e7fcc879fc4ae70344a
Yuval Hayke <hayke102@gmail.com>
2021-07-06 00:22:35 +0300

    data

---
+++ src/todosData.js
@@ -0,0 +1,6 @@
+export const INITIAL_TODOS = [
+    { id: 1, title: "My first todo", done: false },
+    { id: 2, title: "Do Pakalim 💥", done: true },
+    { id: 3, title: "Do 10 final exercises", done: false },
+    { id: 4, title: "Cake break 🎂", done: false },
+];
